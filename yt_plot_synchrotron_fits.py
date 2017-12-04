daa0a1ddcd3c636a58ea735e83c823df972a51ae
Yi-Hao Chen <ychen@astro.wisc.edu>
2017-12-04 15:32:34 -0600

    Save all frequencies in a single fits file

--- yt_plot_synchrotron_fits.py
+++ yt_plot_synchrotron_fits.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import pdb
 import os
 import sys
 import matplotlib
@@ -7,35 +6,36 @@ matplotlib.use('Agg')
 import yt
 from yt_synchrotron_emissivity import *
 yt.enable_parallelism()
-import logging
-from yt import FITSProjection, FITSSlice
-logging.getLogger('yt').setLevel(logging.INFO)
-from yt.utilities.file_handler import HDF5FileHandler
-from yt.funcs import mylog
+yt.mylog.setLevel('INFO')
+from yt import FITSProjection, FITSOffAxisProjection
+from synchrotron.yt_synchrotron_emissivity import\
+        setup_part_file,\
+        write_synchrotron_hdf5,\
+        synchrotron_file_name,\
+        StokesFieldName
 
-def setup_part_file(ds):
-    filename = os.path.join(ds.directory,ds.basename)
-    ds._particle_handle = HDF5FileHandler(filename.replace('plt_cnt', 'part')+'_updated')
-    ds.particle_filename = filename.replace('plt_cnt', 'part')+'_updated'
-    mylog.info('Changed particle files to:' + ds.particle_filename)
 
-#dir = '/home/ychen/data/0only_0529_h1/'
-dir = './'
+dir = '/d/d5/ychen/2015_production_runs/0204_h0_10Myr'
+#dir = './'
 #dir = '/home/ychen/data/0only_0605_h0/'
 #dir = '/home/ychen/data/0only_1022_h1_10Myr/'
+#dir = '/d/d5/ychen/2015_production_runs/1022_h1_10Myr'
 #ts = yt.DatasetSeries(os.path.join(dir,'*_hdf5_plt_cnt_0640'), parallel=1, setup_function=setup_part_file)
 #ts = yt.DatasetSeries(os.path.join(dir,'*_hdf5_plt_cnt_0910'), parallel=1, setup_function=setup_part_file)
 try:
     ind = int(sys.argv[1])
-    ts = yt.DatasetSeries(os.path.join(dir,'*_hdf5_plt_cnt_%02d00' % ind), parallel=1, setup_function=setup_part_file)
+    ts = yt.DatasetSeries(os.path.join(dir,'*_hdf5_plt_cnt_%03d0' % ind), parallel=1, setup_function=setup_part_file)
 except IndexError:
-    ts = yt.DatasetSeries(os.path.join(dir,'*_hdf5_plt_cnt_??0'), parallel=1, setup_function=setup_part_file)
+    ts = yt.DatasetSeries(os.path.join(dir,'*_hdf5_plt_cnt_???0'), parallel=1, setup_function=setup_part_file)
 
 
 zoom_fac = 8
 
+proj_axis = [1,0,2]
+#proj_axis = 'x'
 ptype = 'lobe'
-maindir = os.path.join(dir, 'dtau_synchrotron_QU_nn_%s/' % ptype)
+gc = 32
+maindir = os.path.join(dir, 'cos_synchrotron_QU_nn_%s/' % ptype)
 fitsdir = os.path.join(maindir, 'fits/')
 if yt.is_root():
     for subdir in [maindir, fitsdir]:
@@ -43,18 +43,29 @@ if yt.is_root():
             os.mkdir(subdir)
 
 for ds in ts.piter():
-    proj_axis = 'x'
     width = ds.domain_width[1:]/zoom_fac
-    res = ds.domain_dimensions[1:]*ds.refine_by**ds.index.max_level/zoom_fac
+    res = ds.domain_dimensions[1:]*ds.refine_by**ds.index.max_level//zoom_fac//2
 
     #for nu in [(150, 'MHz')]:
-    for nu in [(150, 'MHz'), (1400, 'MHz')]:
-    ##for nu in [(233, 'MHz'), (325, 'MHz'), (610, 'MHz'), (1.4, 'GHz')]:
-        pars = add_synchrotron_dtau_emissivity(ds, ptype=ptype, nu=nu, proj_axis=proj_axis, extend_cells=8)
-        for pol in ['i', 'q', 'u']:
-            field = ('nn_emissivity_%s_%s_%%.1f%%s' % (pol, ptype)) % nu
-
-            fits_proj = FITSProjection(ds, proj_axis, field, center=[0,0,0], width=width, image_res=res)
-
-            fitsfname = os.path.join(fitsdir, ('synchrotron_%s_%s_%s_%%i%%s.fits' % (pol, ptype, ds.basename[-4:])) % nu)
-            fits_proj.writeto(fitsfname, overwrite=True)
+    #for nu in [(150, 'MHz'), (1400, 'MHz')]:
+    fields = []
+    for nu in [(150, 'MHz'), (233, 'MHz'), (325, 'MHz'), (610, 'MHz'), (1400, 'MHz')]:
+        stokes = StokesFieldName(ptype, nu, proj_axis, field_type='flash')
+        fields.append(stokes.I)
+    #pars = add_synchrotron_dtau_emissivity(ds, ptype=ptype, nu=nu, proj_axis=proj_axis, extend_cells=8)
+    ds_sync = yt.load(synchrotron_file_name(ds, extend_cells=gc))
+    ds_sync.field_list
+    for field in fields:
+        ds_sync.field_info[field].units = 'Jy/cm/arcsec**2'
+        ds_sync.field_info[field].output_units = 'Jy/cm/arcsec**2'
+    if proj_axis != 'x':
+        fits_proj = FITSOffAxisProjection(ds_sync, proj_axis, fields,
+                center=[0,0,0], north_vector=[1,0,0], width=width, image_res=res)
+        fitsfname = 'synchrotron_%s_%i_%i_%i_%s.fits' %\
+                (ptype, *proj_axis, ds.basename[-4:])
+    else:
+        fits_proj = FITSProjection(ds_sync, proj_axis, fields,
+                center=[0,0,0], width=width, image_res=res)
+        fitsfname = 'synchrotron_%s_%s.fits' % (ptype, ds.basename[-4:])
+    fitsfname = os.path.join(fitsdir, fitsfname)
+    fits_proj.writeto(fitsfname, clobber=True)
