636576a88e81075f8a8ca6d18e09eb92b0c67c86
Yi-Hao Chen <ychen@astro.wisc.edu>
2017-12-22 15:20:08 -0600

    Use FITSImageData and construct proper wcs coordinates

--- yt_plot_synchrotron_fits.py
+++ yt_plot_synchrotron_fits.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 import os
 import sys
+import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import yt
-from yt_synchrotron_emissivity import *
 yt.enable_parallelism()
 yt.mylog.setLevel('INFO')
-from yt import FITSProjection, FITSOffAxisProjection
+from itertools import chain
+from yt import FITSImageData
+from yt.visualization.volume_rendering.off_axis_projection import \
+        off_axis_projection
+from astropy.wcs import WCS
 from synchrotron.yt_synchrotron_emissivity import\
         setup_part_file,\
         write_synchrotron_hdf5,\
-        synchrotron_file_name,\
+        synchrotron_filename,\
+        synchrotron_fits_filename,\
         StokesFieldName
 
 
@@ -28,9 +33,15 @@ try:
 except IndexError:
     ts = yt.DatasetSeries(os.path.join(dir,'data/*_hdf5_plt_cnt_???0'), parallel=5, setup_function=setup_part_file)
 
+# Assumed distance to the object
+dist_obj = 165.95*yt.units.Mpc
+# Assumed coordinate of the object
+coord = [229.5, 42.82]
+
+nus = [(nu, 'MHz') for nu in chain(range(100,200,25), range(200,900,50), range(900,1500,100))]
+#nus = [(nu, 'MHz') for nu in [100,150,300,600,1400,8000]]
 
 zoom_fac = 8
-
 proj_axis = [1,0,2]
 #proj_axis = 'x'
 ptype = 'lobe'
@@ -43,30 +54,86 @@ if yt.is_root():
             os.mkdir(subdir)
 
 for ds in ts.piter():
-    width = ds.domain_width[1:]/zoom_fac
-    res = ds.domain_dimensions[1:]*ds.refine_by**ds.index.max_level//zoom_fac
-
-    #for nu in [(150, 'MHz')]:
     fields = []
-    #for nu in [(150, 'MHz')]:
-    for nu in [(150, 'MHz'), (1400, 'MHz')]:
-    #for nu in [(150, 'MHz'), (233, 'MHz'), (325, 'MHz'), (610, 'MHz'), (1400, 'MHz')]:
+    ds_sync = yt.load(synchrotron_filename(ds, extend_cells=gc))
+    ds_sync.field_list
+
+    width = ds_sync.domain_width[1:]/zoom_fac
+    #res = ds_sync.domain_dimensions[1:]*ds_sync.refine_by**ds_sync.index.max_level//zoom_fac
+    res = [512, 1024] if zoom_fac == 8 else [1024, 2048]
+
+    rad = yt.units.rad
+    cdelt1 = (width[0]/dist_obj/res[0]*rad).in_units('deg')
+    cdelt2 = (width[1]/dist_obj/res[1]*rad).in_units('deg')
+
+    # Setting up wcs header
+    w = WCS(naxis=2)
+    # reference pixel coordinate
+    w.wcs.crpix = [res[0]/2,res[1]/2]
+    # sizes of the pixel in degrees
+    w.wcs.cdelt = [cdelt1.base, cdelt2.base]
+    # converting ra and dec into degrees
+    w.wcs.crval = coord
+    # the units of the axes are in degrees
+    w.wcs.cunit = ['deg']*2
+    w.wcs.equinox = 2000
+    wcs_header = w.to_header()
+
+    # Assuming beam area = 1 pixel^2
+    beam_area = cdelt1*cdelt2
+    beam_axis = np.sqrt(beam_area/2/np.pi)*2*np.sqrt(2*np.log(2))
+    # Major and minor beam axes
+    beam_axis = float(beam_axis.in_units('deg').v)
+
+    header_dict = {
+               'CTYPE1': 'RA---SIN',
+               'CTYPE2': 'DEC--SIN',
+               'CROTA1': (0, 'Rotation in degrees.'),
+               'CROTA2': (0, 'Rotation in degrees.'),
+               'CTYPE3': 'FREQ',
+               'CUNIT3': 'Hz',
+               'BMAJ': (beam_axis, 'Beam major axis (deg)'),
+               'BMIN': (beam_axis, 'Beam minor axis (deg)'),
+               'BPA': (0.0, 'Beam position angle (deg)')
+              }
+    for nu in nus:
         stokes = StokesFieldName(ptype, nu, proj_axis, field_type='flash')
         fields += stokes.IQU
-    #pars = add_synchrotron_dtau_emissivity(ds, ptype=ptype, nu=nu, proj_axis=proj_axis, extend_cells=8)
-    ds_sync = yt.load(synchrotron_file_name(ds, extend_cells=gc))
-    ds_sync.field_list
-    for field in fields:
-        try:
+        #fields.append(stokes.I)
+        ds_sync.unit_registry.add('beam', float(beam_area.in_units('rad**2').v),
+                          dimensions=yt.units.dimensions.solid_angle, tex_repr='beam')
+        for field in stokes.IQU:
             ds_sync.field_info[field].units = 'Jy/cm/arcsec**2'
             ds_sync.field_info[field].output_units = 'Jy/cm/arcsec**2'
-        except KeyError:
-            print('Cannot find field:', field, 'in', ds_sync)
     if proj_axis in ['x', 'y', 'z']:
-        fits_proj = FITSProjection(ds_sync, proj_axis, fields,
-                center=[0,0,0], width=width, image_res=res)
+        prj = ds_sync.proj(stokes.I, proj_axis)
+        frb = prj.to_frb(width[0], res, height=width[1])
+        fits_image = FITSImageData(frb, fields=fields, wcs=w)
     else:
-        fits_proj = FITSOffAxisProjection(ds_sync, proj_axis, fields,
-                center=[0,0,0], north_vector=[1,0,0], width=width, image_res=res)
+        buf = {}
+        width = ds_sync.coordinates.sanitize_width(proj_axis, width, (1.0, 'unitary'))
+        wd = tuple(w.in_units('code_length').v for w in width)
+        for field in fields:
+            buf[field] = off_axis_projection(ds_sync, [0,0,0], proj_axis, wd,
+                            res, field, north_vector=[1,0,0], num_threads=0).swapaxes(0,1)
+        fits_image = FITSImageData(buf, fields=fields, wcs=w)
+    for nu in nus:
+        stokes = StokesFieldName(ptype, nu, proj_axis, field_type='flash')
+        field = stokes.I[1]
+        fits_image[field].data.units.registry.add('beam', float(beam_area.in_units('rad**2').v),
+                          dimensions=yt.units.dimensions.solid_angle, tex_repr='beam')
+        fits_image.set_unit(field, 'Jy/beam')
+        nu = yt.YTQuantity(*nu)
+        header_dict.update({
+               'OBJECT': 'Simulation %i %s' % (nu.v, nu.units),
+               'CRVAL3': int(nu.in_units('Hz').v)
+                })
+        fits_image[field].header.update(header_dict)
+        fits_image[field].header.update(wcs_header)
+    #    fits_proj = FITSProjection(ds_sync, proj_axis, fields,
+    #            center=[0,0,0], width=width, image_res=res)
+    #else:
+    #    fits_proj = FITSOffAxisProjection(ds_sync, proj_axis, fields,
+    #            center=[0,0,0], north_vector=[1,0,0], width=width, image_res=res)
     fitsfname = synchrotron_fits_filename(ds, dir, ptype, proj_axis)
-    fits_proj.writeto(fitsfname, clobber=True)
+    fits_image.writeto(fitsfname, clobber=True)
