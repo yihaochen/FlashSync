c29ef36f1b26bf12aecb324479958c4e82186fa7
Yi-Hao Chen <ychen@astro.wisc.edu>
2018-01-22 17:04:27 -0600

    Add more comments

--- yt_write_synchrotron_hdf5.py
+++ yt_write_synchrotron_hdf5.py
@@ -21,9 +21,11 @@ for ds in ts.piter():
     #sync.write_synchrotron_hdf5(ds, 'jetp', (150, 'MHz'), 'x', extend_cells=0)
     #sync.write_synchrotron_hdf5(ds, 'lobe', (150, 'MHz'), 'x', extend_cells=32)
     #sync.write_synchrotron_hdf5(ds, 'lobe', (1.4, 'GHz'), 'x', extend_cells=32)
-    nus = chain(range(100,200,25), range(200,900,50), range(900,1500,100))
-    #nus = [100,150,300,600,1400,8000]
+    #nus = chain(range(100,200,25), range(200,900,50), range(900,1500,100))
+    nus = [100,150,300,600,1400,8000]
     for nu in [(nu, 'MHz') for nu in nus]:
     #for nu in [(150, 'MHz'), (1400, 'MHz')]:
+        # The two projection axes cannot be completed at the same time
+        # Remember to comment out one of the following lines
         #sync.write_synchrotron_hdf5(ds, 'lobe', nu, [1,0,2], extend_cells=32, sanitize_fieldnames=True)
         sync.write_synchrotron_hdf5(ds, 'lobe', nu, 'x', extend_cells=32, sanitize_fieldnames=True)
